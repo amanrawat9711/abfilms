d57743125600659a971c2ba3490d88090240493d
Aman singh rawat <85890950+amanrawat9711@users.noreply.github.com>
2025-06-06 16:43:52 +0530

    Update StudentRegisteration.jsx

--- src/shared/StudentRegisteration.jsx
+++ src/shared/StudentRegisteration.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const StudentRegistrationForm = () => {
     return (
         <div >
-            <h3 className="text-3xl text-center font-bold text-gray-900">Student Registration Form</h3>
+            <h3 className="text-3xl text-center font-bold text-gray-900">Junior Registration Form</h3>
       <form
         className="space-y-4 max-w-xl mx-auto mt-6"
         action="https://formsubmit.co/abfilmindia@gmail.com"
@@ -13,7 +13,7 @@ const StudentRegistrationForm = () => {
         <input type="email" name="email" placeholder="Email" className="w-full p-2 border rounded" required />
         <input type="tel" name="phone" placeholder="Phone Number" className="w-full p-2 border rounded" required />
         <select name="course" className="w-full p-2 border rounded" required>
-          <option value="">Select Course</option>
+          <option value="">Select Field</option>
           <option>Acting</option>
           <option>Direction</option>
           <option>Editing</option>
